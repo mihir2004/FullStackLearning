272208c94f5ff4eca6c8c3f3afe2164a78ece5ed
Mihir K <kasaremihir2004@gmail.com>
2024-06-12 13:15:53 +0530

    ejs

--- Backend/4.4 Band Generator Project/index.js
+++ Backend/4.4 Band Generator Project/index.js
@@ -15,7 +15,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
